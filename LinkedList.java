f4ae9fa80ce2150bb0cf9ff7ddc99cd05484a76d
RyamL1221 <RyamL1221@gmail.com>
2024-05-03 18:14:28 -0400

    Made LinkedList and Node generic types

--- LinkedList.java
+++ LinkedList.java
@@ -1,28 +1,15 @@
-public class LinkedList {
-    private Node root;
+public class LinkedList<T> {
+    private Node<T> root;
+    private int n;
 
-    public Node getRoot() { return root; }
-    public void setRoot(Node root) { this.root = root; }
-
-    class Node {
-        private int data;
-        private Node next;
-
-        public Node(int data) {
-            this.data = data;
-        }
-
-        public int getData() { return data; }
-        public void setData(int data) { this.data = data; }
-        public Node getNext() { return next; }
-        public void setNext(Node next) { this.next = next; }
-    }
+    public Node<T> getRoot() { return root; }
+    public void setRoot(Node<T> root) { this.root = root; }
 
     /*
      * Appends node to end
      * @param node - node that will be appended at the end
      */
-    public void add(Node node) {
+    public void add(Node<T> node) {
         if(node == null) {
             return;
         }
@@ -30,36 +17,68 @@ public class LinkedList {
             root = node; 
             return;
         }
-        Node curr = node;
+        Node<T> curr = node;
         while(curr.getNext() != null) {
             curr = curr.getNext();
         }
         curr.setNext(node);
+        n++;
     }
 
-    public void addToFront(Node node) {
+    public void addToFront(Node<T> node) {
         node.setNext(root);
         root = node;
+        n++;
     }
 
-    public Node search(int data) {
-        Node curr = root;
+    public Node<T> search(T data) {
+        Node<T> curr = root;
         while(curr != null) {
             if(curr.getData() == data) return curr;
         }
         return null;
     }
 
-    public void delete(int data) {
-        Node curr = root;
-        Node prev = curr;
+    public Node<T> search(int index) {
+        if(index >= n) return null;
+        int currIndex = 0;
+        Node<T> curr = root;
+        while(currIndex != index) {
+            currIndex++;
+            curr = curr.getNext();
+        }
+        return curr;
+    }
+
+    public Node<T> delete(T data) {
+        Node<T> curr = root;
+        Node<T> prev = curr;
         while(curr != null) {
-            if(curr.getData() == data) {
+            if(curr.getData().equals(data)) {
+                Node<T> temp = curr;
                 prev.setNext(curr.getNext());
-                return;
+                return temp;
             }
             prev = curr;
             curr = curr.getNext();
         }
-    }       
+        return null;
+    }
+    
+    public Node<T> delete(int index) {
+        if(index >= n) {
+            return null;
+        }
+        Node<T> curr = root; 
+        Node<T> prev = curr;
+        int currIndex = 0; 
+        while(currIndex != index) {
+            prev = curr;
+            curr = curr.getNext();
+            currIndex++;
+        }
+        Node<T> temp = curr;
+        prev.setNext(curr.getNext());
+        return temp;
+    }
 }
