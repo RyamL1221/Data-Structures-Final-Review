3b659b5b3af013f90017d0d8ba7e168800e72232
RyamL1221 <RyamL1221@gmail.com>
2024-05-03 15:54:29 -0400

    Finished QuickFind

--- UnionFind.java
+++ UnionFind.java
@@ -1,22 +1,80 @@
 public class UnionFind {
     
+    /*
+     * Eager approach
+     * Union is too expensive.
+     * Processing a sequence of of n union operations on n elements takes more than n^2 array accesses.
+     * Big O - initialize - O(n), union - O(n), find - O(1)
+     */
     class QuickFind {
         
-        private int[] id;
+        // id[n] points to n's parent 
+        private int[] parent; 
 
+        /*
+         * @param n - array size 
+         */
         public QuickFind(int n) {
-            id = new int[n];
-            for(int i = 0; i < id.length; i++) {
-                id[i] = i;
+            parent = new int[n];
+            for(int i = 0; i < parent.length; i++) {
+                parent[i] = i;
             }
         }
 
+        /*
+         * @param target - the number that is being searched for 
+         * @return the target's parent
+         */
         public int find(int target) {
-            return id[target];
+            return parent[target]; // returns target's parent 
         }
 
+        /*
+         * sets int1 as int2's child
+         * @param int1 - first number that is being unioned
+         * @param int2 - second number that is being unioned 
+         * connecting these two numbers
+         */
         public void union(int int1, int int2) {
-            
+            int parentInt1 = parent[int1]; // finds int1's parent
+            int parentInt2 = parent[int2]; // find int2's parent
+            for(int i = 0; i < parent.length; i++) {
+                if(parent[i] == int1) {
+                    parent[i] = parentInt2;
+                }
+            }
+        }
+    }
+
+    /*
+     * The lazy approach
+     * Can be interpreted as a tree
+     */
+    class QuickUnion {
+
+        // parent array
+        private int[] parent;
+
+        /*
+         * @param n - size of array
+         * Populates the parent array and set each element's parent to itself
+         */
+        public QuickUnion(int n) {
+            parent = new int[n];
+            for(int i = 0; i < parent.length; i++) {
+                parent[i] = i;
+            }
+        }
+
+        /*
+         * @param number that is being searched for
+         * 
+         */
+        public int find(int target) {
+            while(target != parent[target]) {
+                target = parent[target];
+            }
+            return target;
         }
     }
 }
