9ffab655caef6c01ad555d1195e4a15747dd438b
RyamL1221 <RyamL1221@gmail.com>
2024-05-03 20:21:01 -0400

    Finalized Queue class

--- Queue.java
+++ Queue.java
@@ -1,3 +1,33 @@
-public class Queue {
+public class Queue<T> {
+
+    private LinkedList<T> linkedList;
+
+    //size of linked list 
+    private int n;
+
+    // default constructor
+    public Queue() {
+        linkedList = new LinkedList<T>();
+    }
     
+    public boolean isEmpty() { 
+        if(n == 0) return true;
+        return false;
+    }
+    
+    public void enqueue(Node<T> node) {
+        linkedList.add(node);
+    }
+
+    public Node<T> dequeue() {
+        return linkedList.delete(n);
+    }
+
+    public Node<T> peek() {
+        return linkedList.search(0);
+    }
+
+    public int size() {
+        return n;
+    }
 }
