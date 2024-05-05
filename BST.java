e94770c27aef9d29f2729ccdfd2976acd11fa1cd
RyamL1221 <RyamL1221@gmail.com>
2024-05-04 21:44:54 -0400

    Finished BST class

--- BST.java
+++ BST.java
@@ -19,4 +19,46 @@ public class BST<T extends Comparable<T>> {
         if(wentRight) prev.setRight(node);
         else prev.setLeft(node);
     }
+
+    public Node<T> search(T data) {
+        Node<T> curr = root;
+        Node<T> prev = curr;
+        while(curr != null) {
+            prev = curr;
+            int comparison = data.compareTo(curr.getData());
+            if(comparison < 0) curr = curr.getLeft();
+            else if(comparison == 0) return curr;
+            else curr = curr.getRight();
+        }
+        return null; 
+    }
+
+    public void delete(T data) {
+        Node<T> delete = search(data);
+        int numChild = 0;
+        if(delete.getRight() == null) numChild++;
+        if(delete.getLeft() == null) numChild++;
+        if(numChild == 0) delete = null;
+        else if(numChild == 1) {
+            if(delete.getRight() != null) {
+                delete.setData(delete.getRight().getData());
+                delete.setRight(delete.getRight().getRight());
+                delete.setLeft(delete.getRight().getLeft());
+            } else {
+                delete.setData(delete.getLeft().getData());
+                delete.setRight(delete.getLeft().getRight());
+                delete.setLeft(delete.getLeft().getLeft());
+            }
+        } 
+        else {
+            Node<T> inOrderSuccessor = delete.getRight();
+            while(inOrderSuccessor.getLeft() != null) {
+                inOrderSuccessor = inOrderSuccessor.getLeft();
+            }
+            T temp = delete.getData();
+            delete.setData(inOrderSuccessor.getData());
+            inOrderSuccessor.setData(temp);
+            delete(inOrderSuccessor.getData());
+        }
+    }
 }
